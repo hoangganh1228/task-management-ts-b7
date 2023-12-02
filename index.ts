1d7b98844d7b46939881cab412265357c9016a52
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-02 14:41:51 +0700

    3. Đổi trạng thái một công việc

--- index.ts
+++ index.ts
@@ -2,13 +2,16 @@ import express, { Express } from "express";
 import dotenv from "dotenv"
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
-
+import bodyParser from "body-parser";
 dotenv.config();
 
 database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;   
+
+// parse application/json
+app.use(bodyParser.json());
 
 mainV1Routes(app);
 
