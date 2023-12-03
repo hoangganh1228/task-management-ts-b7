0ba24096791f67f939cc43aea7eae8d9a19ea9e1
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-03 09:47:46 +0700

    14. Chuyển các route thành private

--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as controller from "../controllers/user.controller";
+import * as authMiddleware from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
@@ -7,6 +8,6 @@ router.post("/register", controller.register);
 
 router.post("/login", controller.login);
 
-router.get("/detail/:id", controller.detail);
+router.get("/detail", authMiddleware.requireAuth, controller.detail);
 
 export const userRoutes: Router = router;
